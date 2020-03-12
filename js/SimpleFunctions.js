d03004d4dc1eb88ad363c68f599c8daf07395dda
Currysos <nohj.gbg@gmail.com>
2020-03-12 10:39:32 +0100

    Animations

--- js/SimpleFunctions.js
+++ js/SimpleFunctions.js
@@ -2,3 +2,27 @@ function ClassToggle(x) {
     x.classList.toggle("change");
     document.getElementById("Header").classList.toggle("Close");
 }
+
+function ToggleAndroidHeight() {
+    console.log("Toggeling Android");
+
+    document.getElementById('AndroidDownloadSteps').style.height = "250px";
+    document.getElementById('AppleDownloadSteps').style.height = "0px";
+    document.getElementById('WindowsDownloadSteps').style.height = "0px";
+}
+
+function ToggleAppleHeight() {
+    console.log("Toggeling Apple");
+
+    document.getElementById('AndroidDownloadSteps').style.height = "0px";
+    document.getElementById('AppleDownloadSteps').style.height = "250px";
+    document.getElementById('WindowsDownloadSteps').style.height = "0px";
+}
+
+function ToggleWindowsHeight() {
+    console.log("Toggeling Windows");
+
+    document.getElementById('AndroidDownloadSteps').style.height = "0px";
+    document.getElementById('AppleDownloadSteps').style.height = "0px";
+    document.getElementById('WindowsDownloadSteps').style.height = "250px";
+}
